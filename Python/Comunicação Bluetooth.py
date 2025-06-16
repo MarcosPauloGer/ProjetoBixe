45a4a7ebfac9d6ef4141f1532bd89bba993f8f31
Marcos <marcospauloger4@gmail.com>
2025-06-16 17:24:25 -0300

    comentarios no código em python e melhora do READE.ME

---
+++ Python/Comunicação Bluetooth.py
@@ -0,0 +1,35 @@
+import serial            #biblioteca necessária para a comunicação via blueetooth
+import time              #biblioteca necessária para a gestão do tempo no python
+
+#determina a porta serial fictícia usada para a comunicação com o arduino
+serial_port = "COM5"
+#determina a frequência usada para a comuicação com o arduino 
+baud_rate = 9600
+
+#configura a variável 'arduino' a partir dos parâmetros anteriores(a porta de comunicação, a frequencia e o intervalo)
+arduino = serial.Serial(serial_port, baud_rate, timeout = 1)
+time.sleep(2)
+
+#tenta rodar o código
+try:
+    while True:
+        #variável que recebe a mensagem pelo terminal
+        entrada = input('Insira o comando: ')
+        #variável que guarda a mensagem a ser enviada para o ESP32 
+        mensagem = '0'
+        
+        #condição que verifica a direção digitada no terminal e envia ser respecctivo código caso a mensagem esteja correta
+        if entrada == 'direita':
+            arduino.write(mensagem.encode())
+        elif entrada == 'esquerda':
+            mensagem = 1
+            arduino.write(mensagem.encode())
+        else:
+            print('Reescreva o comando: ')
+        
+        #fecha a comunicação momentaneamente, envia o dado e reinicia o ciclo
+        arduino.close()
+
+#caso algum erro ocorra, avisa o usuário
+except serial.SerialException:
+    print('O bluetooth não foi conectado')
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ProjetoBixe
 Repositório usado para o treinamento no GER
+
+# Pasta Python
+Usada para guardar o(s) arquivo(s) em python usado para enviar os dados
+Detalhes do funcionamento do programa estão/deverão estar presentes em comentários no código
+
+# Pasta ESP32
+Usada para guardar o(s) arquivo(s) relacionado(s) ao ESP32
+Detalhes sobre seu funcionamento estão/deverão estar presentes em comentários no código
