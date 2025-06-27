b268a3dd7940077f538dfa3890a389a2f1ac16bf
Marcos <marcospauloger4@gmail.com>
2025-06-27 16:54:55 -0300

    Alterações no código em python

--- Python/Comunicação Bluetooth.py
+++ Python/Comunicação Bluetooth.py
@@ -2,7 +2,7 @@ import serial            #biblioteca necessária para a comunicação via blueet
 import time              #biblioteca necessária para a gestão do tempo no python
 
 #determina a porta serial fictícia usada para a comunicação com o arduino
-serial_port = "COM9"
+serial_port = "COM11"
 #determina a frequência usada para a comuicação com o arduino 
 baud_rate = 9600
 
